61e33a7414d3170068aae297c930b4959d1d2b4a
Kshitiz Raj Paudyal <56644007+Kshitiz726@users.noreply.github.com>
2023-06-14 08:23:10 +0545

    june-14

---
+++ preprocess_image.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(void)
+{
+    //opening the folder of MNIST dataset
+    vector <vector<double>> images;
+    //create a vector with paths of different folder
+    vector <string> folder_paths = {
+        //path of image folder containing number 0
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\0",
+        //path of image folder containing number 1
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\1",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\2",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\3",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\4",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\5",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\6",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\7",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\8",
+        "C:\\Users\\Kshitiz\\Downloads\\mnist_png\\training\\9"
+    };
+    for(int folder_index = 0; folder_index < 10; folder_index++)
+    {
+        string folder_path = folder_paths[folder_index];
+    }
+    }
